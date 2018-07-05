94f2c3a407e466ac8317baa385861d7f0bebb988
admin <admin@admin-HP>
2018-07-05 16:37:50 +0900

    updateAdminLoginServlet - takeda

--- ComicECSite/src/jp/co/axiz/servlet/AdminLoginServlet.java
+++ ComicECSite/src/jp/co/axiz/servlet/AdminLoginServlet.java
@@ -20,26 +20,6 @@ public class AdminLoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-<<<<<<< HEAD
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("id");
-		String pass = request.getParameter("pass");
-
-		AdminInfoDao adminInfiDao = new AdminInfoDao();
-		Admin admin = adminInfoDao.findByIdAndPassword(id, pass);
-//Daoの完成によってメソッド名、変更あり！！
-
-		if (admin != null) {
-			HttpSession session = request.getSession();
-			session.setAttribute("user", admin);
-			response.sendRedirect(request.getContextPath() + "/AdminTop.jsp");
-		} else {
-			request.setAttribute("errmsg", "IDまたはPASSが間違っています");
-			request.getRequestDispatcher("AdminLogin.jsp").forward(request, response);
-		}
-	}
-
-=======
 //	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		String id = request.getParameter("id");
 //		String pass = request.getParameter("pass");
@@ -47,6 +27,7 @@ public class AdminLoginServlet extends HttpServlet {
 //		AdminInfoDao adminInfiDao = new AdminInfoDao();
 //		Admin admin = adminInfoDao.findByIdAndPassword(id, pass);
 //
+//		//Daoの完成によってメソッド名、変更あり！！
 //		if (admin != null) {
 //			HttpSession session = request.getSession();
 //			session.setAttribute("user", admin);
@@ -56,5 +37,4 @@ public class AdminLoginServlet extends HttpServlet {
 //			request.getRequestDispatcher("AdminLogin.jsp").forward(request, response);
 //		}
 //	}
->>>>>>> branch 'master' of https://github.com/Takeda-Axiz/B_group_Project_Axiz.git
 }
